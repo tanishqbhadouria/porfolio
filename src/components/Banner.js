ca365e9e5f3d2acef8e38f1aa0e8df1c838ebc5b
Tanishq Bhadouria <tanishqaingh007@gmail.com>
2024-07-26 23:21:42 +0530

    Initial Commit

---
+++ src/components/Banner.js
@@ -0,0 +1,64 @@
+import { Container, Row,Col } from "react-bootstrap"
+import {ArrowRightCircle} from "react-bootstrap-icons"
+import { useState,useEffect } from "react";
+import headerImg from '../assets/img/headerImg.svg'
+
+
+export const Banner=()=>{
+    const [loopNum,setLoopNum]=useState(0);
+    const toRotate = ['Web Developer','ML Enthusiast','Tech Enthusiast']
+    const [isDeleting,setIsDeleting]=useState(false);
+    const [txt,setTxt]=useState('');
+    const [index, setIndex] = useState(1);
+    const [delta,setDelta]=useState(300-Math.random()*100)
+    const period=2000;
+
+    useEffect(()=>{ 
+        let ticker = setInterval(()=>{
+            tick();
+         },delta)
+
+         return ()=>{clearInterval(ticker)};
+        },[txt])
+
+        const tick=()=>{
+            let i=loopNum % toRotate.length;
+            let fullText=toRotate[i];
+            let updatedText=isDeleting?fullText.substring(0,txt.length-1):fullText.substring(0,txt.length+1);
+
+            setTxt(updatedText);
+
+            if(isDeleting){
+                setDelta(prevDelta=>prevDelta/2)
+            }
+
+            if (!isDeleting && updatedText === fullText) {
+                setIsDeleting(true);
+                setIndex(prevIndex => prevIndex - 1);
+                setDelta(period);
+              } else if (isDeleting && updatedText === '') {
+                setIsDeleting(false);
+                setLoopNum(loopNum + 1);
+                setIndex(1);
+                setDelta(500);
+              } else {
+                setIndex(prevIndex => prevIndex + 1);
+              }
+            }
+
+    return (
+        <Container className="banner">
+            <Row className="align-items-center">
+                <Col xs={12} md={6} xl={7}>
+                    <span className="tagline">Welcome to my Portfolio</span>
+                    <h1>{`hi I'm Tanishq Bhadouria `}<span>{txt}</span></h1>
+                    <p>I am an aspiring software engineer with a passion for web development and have problem-solving skills.</p>
+                    <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                </Col>
+                <Col xs={12} md={6} xl={5}>
+                    <img src={headerImg} alt="tanishq" />
+                </Col>
+            </Row>
+        </Container>
+    )
+}
---
+++ src/components/navBar.js
@@ -0,0 +1,60 @@
+import { Navbar, Nav,Container } from "react-bootstrap"
+import { useState,useEffect } from "react";
+import logo from '../assets/img/logo.svg';
+import instagram from '../assets/img/instagram.svg';
+import linkedin from '../assets/img/linkedin.svg';
+import github from '../assets/img/github.svg';
+
+
+export const NavBar = () => {
+
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, setScrolled] = useState(false);
+  
+    useEffect(() => {
+      const onScroll = () => {
+        if (window.scrollY > 50) {
+          setScrolled(true);
+        } else {
+          setScrolled(false);
+        }
+      }
+  
+      window.addEventListener("scroll", onScroll);
+  
+      return () => window.removeEventListener("scroll", onScroll);
+    }, [])
+  
+    const onUpdateActiveLInk = (value) => {
+      setActiveLink(value);
+    }
+  
+
+    return   (<Navbar expand="md" className={scrolled?"scrolled":""} >
+    <Container>
+      <Navbar.Brand href="/">
+        <img  src={logo} alt="Logo" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav">
+        <span className="navbar-toggler-icon"></span>
+      </Navbar.Toggle>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+          <Nav.Link href="#home" className={activeLink==='home'?'active navbar-link':'navbar-link'} onClick={()=>onUpdateActiveLInk('home')}>Home</Nav.Link>
+          <Nav.Link href="#skills" className={activeLink==='skills'?'active navbar-link':'navbar-link'} onClick={()=>onUpdateActiveLInk('skills')} >Skills</Nav.Link>
+          <Nav.Link href="#projects" className={activeLink==='projects'?'active navbar-link':'navbar-link'} onClick={()=>onUpdateActiveLInk('projects')}>Projects</Nav.Link>
+        </Nav>
+        <span className="navbar-text">
+          <div className="social-icon">
+            <a href="www.instagram.com"><img src={instagram} alt="" /></a>
+            <a href="www.linkedin.com"><img src={linkedin} alt="" /></a>
+            <a href="www.github.com"><img src={github} alt="" /></a>
+          </div>
+         
+            <button className="vvd"><span>Let’s Connect</span></button>
+          
+        </span>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>)
+}
